76530c933dfa2a79f1ba1dd3b0c8a27811a21e3d
QuanNhat <quannguyennnq2208@gmail.com>
2023-11-06 21:25:40 +0700

    á»origin

---
+++ src/constants/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  // baseUrl: 'https://api-ecom.duthanhduoc.com/',
+  // baseUrl: 'mongodb://localhost:27017/',
+  baseUrl: 'http://localhost:4000/',
+  maxSizeUploadAvatar: 1048576 // bytes
+}
+
+export default config
