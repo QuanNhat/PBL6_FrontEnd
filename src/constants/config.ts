b0307c5c46b417bbce8657b5ab6f052fcf0824eb
QuanNhat <quannguyennnq2208@gmail.com>
2024-01-04 18:45:20 +0700

    2

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,7 +3,7 @@ const config = {
   // baseUrl: 'https://pbl6-backend.com/',
   // baseUrl: 'http://localhost:4000/',
   // baseUrl: 'https://shopee-mobile-app-clone-server.onrender.com/',
-  baseUrl: 'http://192.168.13.34/',
+  baseUrl: 'http://192.168.13.34:4000/',
   maxSizeUploadAvatar: 1048576 // bytes
 }
 
